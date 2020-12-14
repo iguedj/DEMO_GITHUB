6ba659ec2af031f101677397213c525fffb45dff
Ilan Guedj <ilan.guedj.ort@gmail.com>
2020-12-14 20:32:54 +0100

    // DEMO GITHUB

---
+++ src/demo_github/DEMO_GITHUB.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo_github;
+
+/**
+ *
+ * @author ilang
+ */
+public class DEMO_GITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+     // VOILA UNE DEMO
+    //DEMO 2
+}
